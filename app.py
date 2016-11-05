1fc6e34afc26ab5f36a74711008f00a9a65a6632
Shivang Gupta <shivang2005@gmail.com>
2016-11-05 17:00:45 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,79 @@
+#!flask/bin/python
+from flask import Flask
+from flask import jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+target = open('data.txt', 'a')
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+dataset = []
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task2 = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    print task2
+    tasks.append(task2)
+    return jsonify({'task': task2}), 201
+    print "hello"
+
+@app.route('/api/v1.0/dataset', methods=['GET'])
+def get_dataset():
+    return jsonify({'dataset': dataset})
+
+@app.route('/api/v1.0/dataset', methods=['POST'])
+def create_data():
+    if not request.json:
+        abort(400)
+    d = {
+        'data': request.json['data']
+    }
+    print request.json['data']
+    dataset.append(d)
+    target.write(request.json['data'] + "\n")
+    return jsonify({'data': d}), 201
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=80)
